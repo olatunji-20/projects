47556bb242dde6722b09a3bc84c75dd9684dbb3b
falana sheriff <falanasheriffdeen@gmail.com>
2024-01-23 16:45:35 +0100

    a new repo for all subsequent projects

---
+++ gdlive/gdlive/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="border-2 border-red-400 w-[100%] h-[120px]">fif</header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ gdlive/gdlive/src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <Navbar />
+  <h1>LAULAU</h1>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+
+export default {
+    name: "HomePage",
+    components: {
+        Navbar
+    }
+
+}
+</script>
+
+<style>
+
+</style>
