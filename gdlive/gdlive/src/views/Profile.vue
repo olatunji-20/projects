ceea7ba59681aac4cda86d9b9899faec860d62b2
falana sheriff <falanasheriffdeen@gmail.com>
2024-01-30 21:14:34 +0100

    worked on profile page

--- gdlive/gdlive/src/views/Profile.vue
+++ gdlive/gdlive/src/views/Profile.vue
@@ -8,7 +8,28 @@
         </div>
     </main>
 
-    <div class="border-4 border-blue-600 bg-blue-200 w-[100%] h-[150px] my-16 mx-auto md:w-[80%]"></div>
+    <div class="border-4 border-blue-600 bg-blue-200 w-[100%] h-[auto] my-16 mx-auto py-1 md:flex flex-row items-center justify-evenly md:w-[80%]">
+        <p class="font-bold text-left">Progress:</p>
+        <div class="border-2 border-red-300 w-[100%] h-[100px] my-4 flex flex-row flex-nowrap items-center justify-evenly md:w-[70%]">
+
+            <div class="border-2 border-blue-400 w-[33%] h-[80px] flex flex-row items-center md:w-[33%]">
+                <div class="border-2 border-green-500 w-[70px] h-[70px] rounded-full"></div>
+                <p class="pl-1 text-sm md:pl-4">Enrollment</p>
+            </div>
+            
+
+            <div class="border-2 border-blue-400 w-[33%] h-[80px] flex flex-row items-center md:w-[33%]">
+                <div class="border-2 border-green-500 w-[70px] h-[70px] rounded-full"></div>
+                <p class="pl-1 text-sm md:pl-4">Transfer</p>
+            </div>
+
+            <div class="border-2 border-blue-400 w-[33%] h-[80px] flex flex-row items-center md:w-[33%]">
+                <div class="border-2 border-green-500 w-[70px] h-[70px] rounded-full"></div>
+                <p class="pl-1 text-sm md:pl-4">Completed</p>
+            </div>
+
+        </div>
+    </div>
 
     <div class="border-4 border-purple-600 bg-purple-200 w-[100%] h-[1200px] py-8 px-4 my-16 mx-auto mb-48 md:w-[80%]">
         <h3 class="font-bold text-lg">Activities</h3>
