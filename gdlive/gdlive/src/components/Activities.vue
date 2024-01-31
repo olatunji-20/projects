4cd3ece489054bf8a41f915aedcdf175ad02642a
falana sheriff <falanasheriffdeen@gmail.com>
2024-01-31 15:59:50 +0100

    continue

--- gdlive/gdlive/src/components/Activities.vue
+++ gdlive/gdlive/src/components/Activities.vue
@@ -1,8 +1,59 @@
 <template>
-  <div class="border-2 border-red-600 w-[100%] h-[150px] rounded-md bg-yellow-200 my-[30px]"></div>
-  <div class="border-2 border-red-600 w-[100%] h-[150px] rounded-md bg-yellow-200 my-[30px]"></div>
-  <div class="border-2 border-red-600 w-[100%] h-[150px] rounded-md bg-yellow-200 my-[30px]"></div>
-  <div class="border-2 border-red-600 w-[100%] h-[150px] rounded-md bg-yellow-200 my-[30px]"></div>
+    
+  <div class="border-2 border-red-600 w-[100%] h-[150px] rounded-md bg-yellow-200 my-[30px] flex flex-row justify-between">
+    <div class="border-4 border-blue-500 w-[65%] h-[100%] flex flex-row flex-nowrap justify-between">
+        <div class="border-2 border-blue-500 w-[70px] h-[70px] rounded-full"></div>
+        <div class="border-2 border-green-500 w-[70%] h-[100%]">
+            <h3 class="font-bold text-xl">Enrolled</h3>
+            <p class="font-bold mt-4">Location</p>
+            <p class="text-sm font-bold">Lagos, Nigeria</p>
+            <p class="text-sm mt-4">Fri, Feb 12, 2023</p>
+        </div>
+    </div>
+    <div class="border-4 border-red-500 w-[25%] h-[100%]">
+        <p class="hidden md:block">20 hours ago</p>
+        <p class="hidden md:inline-block">Show story</p>
+        <p class="inline ml-4">S</p>
+    </div>
+  </div>
+
+  <div class="border-2 border-red-600 w-[100%] h-[150px] rounded-md bg-yellow-200 my-[30px] flex flex-row justify-between">
+    <div class="border-4 border-blue-500 w-[65%] h-[100%] flex flex-row flex-nowrap justify-between">
+        <div class="border-2 border-blue-500 w-[70px] h-[70px] rounded-full"></div>
+        <div class="border-2 border-green-500 w-[70%] h-[100%]">
+            <h3 class="font-bold text-xl">Initial Payment</h3>
+            <p class="font-bold mt-4">Transfer Amount</p>
+            <p class="text-sm font-bold">55 000KEH ($38 000)</p>
+            <p class="text-sm mt-4">Fri, Feb 12, 2023</p>
+        </div>
+    </div>
+    <div class="border-4 border-red-500 w-[25%] h-[100%]">
+        <p class="hidden md:block">20 hours ago</p>
+        <p class="hidden md:inline-block">Show story</p>
+        <p class="inline ml-4">S</p>
+    </div>
+  </div>
+
+  <div class="border-2 border-red-600 w-[100%] h-[150px] rounded-md bg-yellow-200 my-[30px] flex flex-row justify-between">
+    <div class="border-4 border-blue-500 w-[65%] h-[100%] flex flex-row flex-nowrap justify-between">
+        <div class="border-2 border-blue-500 w-[70px] h-[70px] rounded-full"></div>
+        <div class="border-2 border-green-500 w-[70%] h-[100%]">
+            <h3 class="font-bold text-xl">Enrolled</h3>
+            <p class="font-bold mt-4">Location</p>
+            <p class="text-sm font-bold">Lagos, Nigeria</p>
+            <p class="text-sm mt-4">Fri, Feb 12, 2023</p>
+        </div>
+    </div>
+    <div class="border-4 border-red-500 w-[25%] h-[100%]">
+        <p class="hidden md:block">20 hours ago</p>
+        <p class="hidden md:inline-block">Show story</p>
+        <p class="inline ml-4">S</p>
+    </div>
+  </div>
+
+
+
+
 </template>
 
 <script>
